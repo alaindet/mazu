20da14b810d1e6824b6736985ecd17718a729cb7
Alain D'Ettorre <alain.det@gmail.com>
2022-01-14 07:21:44 +0100

    Update db mock

--- database/mock.sql
+++ database/mock.sql
@@ -11,3 +11,26 @@ INSERT INTO `users` (
   `password`
 ) VALUES
 (1, 'John', 'Doe', 2, 'john.doe@example.com', '$2y$10$FhLNAZnCGIL2F2RsreF2Ruw7a9jo9c1OFvWTIK/n1UC5YtDuyhDrC');
+
+INSERT INTO `lists` (
+  `list_id`,
+  `user_id`,
+  `is_favorite`,
+  `name`,
+  `description`
+) VALUES
+(1, 1, 0, 'Groceries', 'The groceries list');
+
+INSERT INTO `items` (
+  `item_id`,
+  `user_id`,
+  `list_id`,
+  `name`,
+  `amount`
+) VALUES
+(1, 1, 1, 'Water', 6),
+(2, 1, 1, 'Bread', 1),
+(3, 1, 1, 'Milk', 4),
+(4, 1, 1, 'Salad', 2),
+(5, 1, 1, 'Pasta', 3),
+(6, 1, 1, 'chocolate', 2);

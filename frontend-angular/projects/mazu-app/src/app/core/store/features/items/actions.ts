86c83d5cc900790a12433f63b3cdf4bbd0692df5
Alain D'Ettorre <alain.det@gmail.com>
2022-01-20 07:24:34 +0100

    Update NgRx items actions

--- frontend-angular/projects/mazu-app/src/app/core/store/features/items/actions.ts
+++ frontend-angular/projects/mazu-app/src/app/core/store/features/items/actions.ts
@@ -1,45 +1,187 @@
 import { createAction, props } from '@ngrx/store';
 
-export enum ItemsActionType {
-
-  Create = '[Items] Create',
-  CreateSuccess = '[Items] Create success',
-  CreateFailure = '[Items] Create failure',
-
-  GetAll = '[Items] Get all',
-  GetAllSuccess = '[Items] Get all success',
-  GetAllFailure = '[Items] Get all failure',
-
-  UpdateOne = '[Items] Update one',
-  UpdateOneSuccess = '[Items] Update one success',
-  UpdateOneFailure = '[Items] Update one failure',
-
-  MarkOne = '[Items] Mark one',
-  MarkOneSuccess = '[Items] Mark one success',
-  MarkOneFailure = '[Items] Mark one failure',
-
-  UnmarkOne = '[Items] Unmark one',
-  UnmarkOneSuccess = '[Items] Unmark one success',
-  UnmarkOneFailure = '[Items] Unmark one failure',
-
-  DeleteOne = '[Items] Delete one',
-  DeleteOneSuccess = '[Items] Delete one success',
-  DeleteOneFailure = '[Items] Delete one failure',
-
-  MarkAll = '[Items] Mark all',
-  MarkAllSuccess = '[Items] Mark all success',
-  MarkAllFailure = '[Items] Mark all failure',
-
-  UnmarkAll = '[Items] Unmark all',
-  UnmarkAllSuccess = '[Items] Unmark all success',
-  UnmarkAllFailure = '[Items] Unmark all failure',
-
-  DeleteAll = '[Items] Delete all',
-  DeleteAllSuccess = '[Items] Delete all success',
-  DeleteAllFailure = '[Items] Delete all failure',
-
-  DeleteAllMarked = '[Items] Delete all marked',
-  DeleteAllMarkedSuccess = '[Items] Delete all marked success',
-  DeleteAllMarkedFailure = '[Items] Delete all marked failure',
-
+// TODO: Move interfaces away
+export interface CreateItemDto {
+  listId: string;
+  name: string;
+  amount: number;
+  description?: string;
 }
+
+export interface UpdateItemDto {
+  itemId: string;
+  listId: string;
+  name?: string;
+  amount?: number;
+  description?: string;
+  isDone?: boolean;
+}
+
+export interface Item {
+  itemId: string;
+  listId: string;
+  name: string;
+  amount: number;
+  description: string;
+  isDone: boolean;
+}
+
+export interface ImplicitUpdateItemDto {
+  listId: string;
+  itemId: string;
+}
+
+export interface ImplicitUpdateListDto {
+  listId: string;
+}
+
+export interface TextFeedback {
+  message: string;
+}
+
+export const createItem = createAction(
+  '[Items] Create',
+  props<CreateItemDto>(),
+);
+export const createItemSuccess = createAction(
+  '[Items] Create success',
+  props<Item>(),
+);
+export const createItemFailure = createAction(
+  '[Items] Create failure',
+  props<TextFeedback>(),
+);
+
+
+
+export const getAllItems = createAction(
+  '[Items] Get all',
+);
+export const getAllItemsSuccess = createAction(
+  '[Items] Get all success',
+  props<{ items: Item[] }>(),
+);
+export const getAllItemsFailure = createAction(
+  '[Items] Get all failure',
+  props<TextFeedback>(),
+);
+
+
+
+export const updateItem = createAction(
+  '[Items] Update',
+  props<UpdateItemDto>(),
+);
+export const updateItemSuccess = createAction(
+  '[Items] Update success',
+  props<Item>(),
+);
+export const updateItemFailure = createAction(
+  '[Items] Update failure',
+  props<TextFeedback>(),
+);
+
+
+
+export const markItemAsDone = createAction(
+  '[Items] Mark as done',
+  props<ImplicitUpdateItemDto>(),
+);
+export const markItemAsDoneSuccess = createAction(
+  '[Items] Mark as done success',
+  props<TextFeedback>(),
+);
+export const markItemAsDoneFailure = createAction(
+  '[Items] Mark as done failure',
+  props<TextFeedback>(),
+);
+
+
+
+export const unmarkItemAsDone = createAction(
+  '[Items] Unmark as done',
+  props<ImplicitUpdateItemDto>(),
+);
+export const unmarkItemAsDoneSuccess = createAction(
+  '[Items] Unmark as done success',
+  props<TextFeedback>(),
+);
+export const unmarkItemAsDoneFailure = createAction(
+  '[Items] Unmark as done failure',
+  props<TextFeedback>(),
+);
+
+
+
+export const deleteItem = createAction(
+  '[Items] Delete',
+  props<ImplicitUpdateItemDto>(),
+);
+export const deleteItemSuccess = createAction(
+  '[Items] Delete success',
+  props<TextFeedback>(),
+);
+export const deleteItemFailure = createAction(
+  '[Items] Delete failure',
+  props<TextFeedback>(),
+);
+
+
+
+export const markAllItemsAsDone = createAction(
+  '[Items] Mark all as done',
+  props<ImplicitUpdateListDto>(),
+);
+export const markAllItemsAsDoneSuccess = createAction(
+  '[Items] Mark all as done success',
+  props<TextFeedback>(),
+);
+export const markAllItemsAsDoneFailure = createAction(
+  '[Items] Mark all as done failure',
+  props<TextFeedback>(),
+);
+
+
+
+export const unmarkAllItemsAsDone = createAction(
+  '[Items] Unmark all as done',
+  props<ImplicitUpdateListDto>(),
+);
+export const unmarkAllItemsAsDoneSuccess = createAction(
+  '[Items] Unmark all as done success',
+  props<TextFeedback>(),
+);
+export const unmarkAllItemsAsDoneFailure = createAction(
+  '[Items] Unmark all as done failure',
+  props<TextFeedback>(),
+);
+
+
+
+export const deleteAllItems = createAction(
+  '[Items] Delete all',
+  props<ImplicitUpdateListDto>(),
+);
+export const deleteAllItemsSuccess = createAction(
+  '[Items] Delete all success',
+  props<TextFeedback>(),
+);
+export const deleteAllItemsFailure = createAction(
+  '[Items] Delete all failure',
+  props<TextFeedback>(),
+);
+
+
+
+export const deleteAllDoneItems = createAction(
+  '[Items] Delete all done',
+  props<ImplicitUpdateListDto>(),
+);
+export const deleteAllDoneItemsSuccess = createAction(
+  '[Items] Delete all done success',
+  props<TextFeedback>(),
+);
+export const deleteAllDoneItemsFailure = createAction(
+  '[Items] Delete all done failure',
+  props<TextFeedback>(),
+);

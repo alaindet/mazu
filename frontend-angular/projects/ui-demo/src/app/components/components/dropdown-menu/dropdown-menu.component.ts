83016be12e6f124c59be253acb9d72d853ca8ec6
Alain D'Ettorre <alain.det@gmail.com>
2022-02-14 09:10:39 +0100

    Style dropdown-menu example

--- frontend-angular/projects/ui-demo/src/app/components/components/dropdown-menu/dropdown-menu.component.ts
+++ frontend-angular/projects/ui-demo/src/app/components/components/dropdown-menu/dropdown-menu.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './dropdown-menu.component.html',
+  styleUrls: ['./dropdown-menu.component.scss'],
 })
 export class DemoDropdownMenuComponent {
   consoleLog = console.log;
